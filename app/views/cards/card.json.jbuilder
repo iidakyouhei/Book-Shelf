2e2253c9bc3cd9a2fdd5a0e455c8290fad6bd48d
iidakyouhei <kunekune0803@gmail.com>
2020-12-31 18:08:45 +0900

    edit-destory

--- app/views/cards/card.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! card, :id, :image, :created_at, :updated_at
-json.image card_url(card, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'cards#index'
   resources :cards do
-    member do
-      get 'show_image'
-    end
+
   end
 end
